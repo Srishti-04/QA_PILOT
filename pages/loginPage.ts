ded3d674170b5184206335d6c2b5c96541d506ec
Srishti-04 <srishtijaiswal2823@gmail.com>
2026-05-04 00:36:24 +0530

    Refactored POM and fixed CI stability issues

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,21 +1,34 @@
-import { Page } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 
 export class LoginPage {
-  constructor(private page: Page) {}
+  readonly page: Page;
+  readonly username: Locator;
+  readonly password: Locator;
+  readonly loginBtn: Locator;
+  readonly flashMsg: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.username = page.locator('#username');
+    this.password = page.locator('#password');
+    this.loginBtn = page.locator('button[type="submit"]');
+    this.flashMsg = page.locator('#flash');
+  }
 
   async goto() {
-  await this.page.goto('https://the-internet.herokuapp.com/login');
-  await this.page.waitForSelector('#username');
-}
-  async login(username: string, password: string) {
-  await this.page.waitForSelector('#username');
-  await this.username.fill(username);
+    await this.page.goto('https://the-internet.herokuapp.com/login', {
+      waitUntil: 'domcontentloaded',
+      timeout: 60000
+    });
+  }
 
-  await this.password.fill(password);
-  await this.loginBtn.click();
-}
+  async login(username: string, password: string) {
+    await this.username.fill(username);
+    await this.password.fill(password);
+    await this.loginBtn.click();
+  }
 
   async getMessage() {
-    return this.page.locator('#flash').textContent();
+    return await this.flashMsg.textContent();
   }
 }
